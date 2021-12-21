40f932f1b29b19774603a99358e9bbe1dc0962fe
‘17521746738@163.com’ <17521746738@163.com>
2021-12-21 22:02:08 +0800

    init

---
+++ src/main/java/com/zhc/cloud/springclouddemo/SpringcloudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zhc.cloud.springclouddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhc/cloud/springclouddemo/SpringcloudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhc.cloud.springclouddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
