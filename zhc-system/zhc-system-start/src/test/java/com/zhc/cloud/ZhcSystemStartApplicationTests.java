19e9fc2ad3e28980bcce90e9216d2143a74873ae
‘17521746738@163.com’ <17521746738@163.com>
2021-12-23 21:36:19 +0800

    结构配置

---
+++ zhc-system/zhc-system-start/src/test/java/com/zhc/cloud/ZhcSystemStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhc.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhcSystemStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
