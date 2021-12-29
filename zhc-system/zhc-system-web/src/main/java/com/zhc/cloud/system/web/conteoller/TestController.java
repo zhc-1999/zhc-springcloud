de6c6a4d6c29afdb39818ec0e4aabf7e96c79aff
‘17521746738@163.com’ <17521746738@163.com>
2021-12-30 00:28:14 +0800

    更新命名空间ID及注释

--- zhc-system/zhc-system-web/src/main/java/com/zhc/cloud/system/web/conteoller/TestController.java
+++ zhc-system/zhc-system-web/src/main/java/com/zhc/cloud/system/web/conteoller/TestController.java
@@ -5,6 +5,7 @@ import com.zhc.cloud.system.service.TestService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,11 +26,11 @@ public class TestController {
     @Autowired
     private TestDataService testDataService;
 
-//    @GetMapping("test")
-//    public String test(){
-//        log.info("test");
-//        return testDataService.test();
-//    }
+    @GetMapping("test1")
+    public String test1(){
+        log.info("test");
+        return "testDataService.test()";
+    }
 
 
     @RequestMapping(value = "/test", method = RequestMethod.POST)
